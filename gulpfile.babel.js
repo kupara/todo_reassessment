7fadaf08cc083f813208481e1a769893af35614a
Edwin Kupara <edwin.kupara@andela.com>
2017-02-15 14:23:32 +0300

    Application setup complete

---
+++ gulpfile.babel.js
@@ -0,0 +1,75 @@
+import gulp from 'gulp';
+import connect from 'gulp-connect';
+import lint from 'gulp-eslint';
+import babelify from 'babelify';
+import sass from 'gulp-sass';
+import source from 'vinyl-source-stream';
+import browserify from 'browserify';
+import historyFallback from 'connect-history-api-fallback';
+import mocha from 'gulp-mocha';
+
+const config = {
+  port: 3001,
+  baseUrl: 'http://localhost',
+  files: {
+    html: './app/*.html',
+    dist: './dist',
+    sass: './app/styles/*.sass',
+    js: './app/scripts/**/*.js',
+    tests: './app/**/*test.js',
+    mainJs: './app/scripts/main.js'
+  }
+};
+
+gulp.task('connect', () => {
+  connect.server({
+    base: config.baseUrl,
+    root: ['dist'],
+    port: config.port,
+    livereload: true,
+    middleware: () => [historyFallback()]
+  })
+});
+
+gulp.task('js', () => {
+  browserify(config.files.mainJs)
+    .transform(babelify)
+    .bundle()
+    .on('error', console.error.bind(console))
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(`${config.files.dist}/scripts`))
+    .pipe(connect.reload());
+});
+
+gulp.task('html', () => {
+  gulp.src(config.files.html)
+    .pipe(gulp.dest(config.files.dist))
+    .pipe(connect.reload());
+});
+
+gulp.task('sass', () => {
+  gulp.src(config.files.sass)
+    .pipe(sass())
+    .pipe(gulp.dest(`${config.files.dist}/styles`))
+    .pipe(connect.reload());
+});
+
+gulp.task('lint', () => {
+  gulp.src(config.files.js)
+    .pipe(lint())
+    .pipe(lint.format())
+    .pipe(lint.failAfterError());
+});
+
+gulp.task('test', () => {
+  gulp.src(config.files.tests)
+    .pipe(mocha({ reporter: 'dot' }))
+});
+
+gulp.task('watch', () => {
+  gulp.watch(config.files.js, ['js', 'lint']);
+  gulp.watch(config.files.html, ['html']);
+  gulp.watch(config.files.sass, ['sass']);
+});
+
+gulp.task('default', ['js', 'html', 'lint', 'sass', 'connect', 'watch']);
